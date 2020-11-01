b26dc66d74a0c2b7db071f9c0c33c346a2cc541d
amaddatu <>
2020-11-01 14:17:04 -0500

    Configure passport with the mongoose user model

--- models/user.js
+++ models/user.js
@@ -29,11 +29,8 @@ userSchema.pre('save', function(next) {
     });
 });
 
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
+userSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compareSync(candidatePassword, this.password);
 };
 
 const User = mongoose.model("User", userSchema);

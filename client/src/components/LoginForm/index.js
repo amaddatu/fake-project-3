8ec6c31d72d8172323e17b66993c9d701fe1122a
amaddatu <>
2020-11-01 13:41:27 -0500

    Added both front and back

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./style.scss";
+import API from '../../utils/API';
 
 function LoginForm(props) {
     let extraProps = {}
@@ -9,9 +10,20 @@ function LoginForm(props) {
     let emailId = props.className ? props.className + "-login-email": "login-email";
     let emailHelpId = props.className ? props.className + "-login-email-help": "login-email-help";
     let passwordId = props.className ? props.className + "-login-password": "login-password";
-    
+    const handleSubmit = event => {
+        // if the user hits enter or hits the button, this function will fire
+        event.preventDefault();
+        console.log("submit happened");
+        API.testUserRouter()
+        .then( data => {
+            console.log(data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
     return (
-        <form {...extraProps}>
+        <form {...extraProps} onSubmit={handleSubmit}>
             <div className="form-group">
                 <label htmlFor={emailId}>Email address</label>
                 <input type="email" className="form-control" id={emailId} aria-describedby={emailHelpId} />
